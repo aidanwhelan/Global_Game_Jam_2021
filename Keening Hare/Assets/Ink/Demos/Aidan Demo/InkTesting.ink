bd5897c7c71b78318d2c5c47fc7a077180150113
Aidan Whelan <aidanjwhelan@gmail.com>
2021-01-31 10:28:37 -0500

    Added demo Ink file and example script for navigating Story API

---
+++ Keening Hare/Assets/Ink/Demos/Aidan Demo/InkTesting.ink
@@ -0,0 +1,22 @@
+//Example Intro
+- One story is never enough.
+- ‘Tomorrow,’ she promised, ‘I will read you the frightful story of the Keening Hare'.
+- But I couldn’t wait. 
+
+    //Intro Choice 1
+*   I turned left and made my way to the Henge
+
+//Example Area
+    I tripped on a rotting log and stumbled. 
+    * * I brushed myself off and went back home.
+    * * I could not stand, so I accepted my fate.
+    -> END
+
+
+
+    //Intro Choice 2
+*   I turned around and went back home
+
+//END
+- And thus ends our story.
+- -> END
---
+++ Keening Hare/Assets/Scripts/InkTestingScript.cs
@@ -0,0 +1,58 @@
+/*
+InkTestingScript.cs
+
+This is a Demo script to show how a JSON file, compiled from an Ink project,
+can be read in and navigated using the Story API.
+*/
+using Ink.Runtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InkTestingScript : MonoBehaviour
+{
+    //Take in new Ink file, extracting text from .json
+    public TextAsset inkJSON;
+    private Story story;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Create a Story object to navigate JSON text contents with Story API
+        story = new Story(inkJSON.text);
+        Debug.Log(loadStoryChunk() );
+
+        //Example: Display all currently available choices
+        for(int i = 0; i < story.currentChoices.Count; i++)
+        {
+            Debug.Log(story.currentChoices[i].text);
+        }
+
+        //Example: Choose the first choice
+        story.ChooseChoiceIndex(0);
+
+        Debug.Log(loadStoryChunk() );
+
+        //Example: Choose the first choice
+        story.ChooseChoiceIndex(1);
+
+        Debug.Log(loadStoryChunk() );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    string loadStoryChunk()
+    {
+        string text = "";
+        if(story.canContinue)
+        {
+            text = story.ContinueMaximally();
+        }
+
+        return text;
+    }
+}
